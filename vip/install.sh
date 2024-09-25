1346009e1367029ea1d454a7cf602e607dfdda4d
OzZyDEV <khosyividpra@gmail.com>
2024-09-25 20:41:53 +0700

    Update install.sh
    added target package info

--- vip/install.sh
+++ vip/install.sh
@@ -3,5 +3,9 @@ import axeron.prop
 local modcore="https://lrzozz256.github.io/GSpeed/vip/core.sh"
 
 echo "$name | VIP"
+sleep 1
 echo "Thanks For Buy, enjoy!"
+sleep 1
+echo "[ ➤ ] Target Package: $runPackage"
+sleep 2
 storm -x "$modcore" -fn "modcore" "$@"
