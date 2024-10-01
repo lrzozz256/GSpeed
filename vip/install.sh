9b9e40f72b4f3d31074bb08bd5ab0e327f18104b
OzZyDEV <khosyividpra@gmail.com>
2024-10-01 12:00:24 +0700

    Update install.sh
    Added info

--- vip/install.sh
+++ vip/install.sh
@@ -2,6 +2,18 @@ $AXFUN
 import axeron.prop
 local modcore="https://lrzozz256.github.io/GSpeed/vip/core.sh"
 
+case $1 in
+  --info | -i )
+    echo "$name | Information"
+    echo "ID: $trim_id"
+    echo "Plan: VIP"
+    echo "Version: $version($versionCode)"
+    echo "Whoami: $(whoami) | $(id -u)"
+    echo "SELinux: $(getenforce)"
+    exit 0
+    ;;
+esac
+
 echo "$name | VIP"
 sleep 1
 echo "Thanks For Buy, enjoy!"
